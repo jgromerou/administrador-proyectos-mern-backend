4b4c435aaabbdd02b0d71b6e7c26794b6240817f
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-05-01 10:40:56 -0300

    se terminó el socket.io, crear un room, agregar, editar, eliminar y cambiar el estado de una tarea del proyecto

--- index.js
+++ index.js
@@ -38,6 +38,45 @@ app.use('/api/tareas', tareaRoutes);
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
+const servidor = app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
+
+//Socket.io
+import { Server } from 'socket.io';
+
+const io = new Server(servidor, {
+  pingTimeout: 60000,
+  cors: {
+    origin: process.env.FRONTEND_URL,
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log('Conectado a socket.io');
+
+  //Defino los eventos de socket.io
+  // socket.on('prueba', (nombre) => {
+  //   console.log('Mi nombre es: ', nombre);
+  // });
+  socket.on('abrir proyecto', (proyecto) => {
+    socket.join(proyecto);
+    //socket.emit('respuesta', { nombre: 'Juan' });
+  });
+
+  socket.on('nueva tarea', (tarea) => {
+    socket.to(tarea.proyecto).emit('tarea agregada', tarea);
+  });
+
+  socket.on('eliminar tarea', (tarea) => {
+    socket.to(tarea.proyecto).emit('tarea eliminada', tarea);
+  });
+
+  socket.on('actualizar tarea', (tarea) => {
+    socket.to(tarea.proyecto._id).emit('tarea actualizada', tarea);
+  });
+
+  socket.on('cambiar estado', (tarea) => {
+    socket.to(tarea.proyecto._id).emit('nuevo estado', tarea);
+  });
 });
